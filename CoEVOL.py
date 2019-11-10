97e4fdd3533ab4eedf3058d2a057b69a55fe6fee
Alican Buyukcakir <alicanbuyukcakir@gmail.com>
2019-11-10 00:50:38 -0500

    CoEVOL works with sparse matrices now

--- CoEVOL.py
+++ CoEVOL.py
@@ -5,6 +5,9 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt
 from networkx.readwrite.edgelist import read_edgelist
+from scipy.sparse import csr_matrix
+from scipy.sparse import lil_matrix
+import scipy.sparse as sps
 
 class CoEVOL():
     '''
@@ -26,18 +29,20 @@ class CoEVOL():
     '''
 
     def __init__(self, A_T_all, k: int = 10, theta: float = 0.3,
-                 alpha: float = 0.01, beta: float = 0.01, gamma: float = 0.01):
+                 alpha: float = 0.01, beta: float = 0.01, gamma: float = 0.01,
+                 threshold : float = 0.01):
 
         self.A = A_T_all
         self.T = self.A.shape[0]
         self.s = self.A.shape[1]
-        self.n = self.A.shape[2]
+        self.n = self.A[0,0].shape[0]
         self.k = k
 
         self.theta = theta
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
+        self.threshold = threshold
 
         print('CoEVOL model with:\n Number of time steps={}\n \
         Number of Subjects={}\nMatrix Size={} '.format(self.T, self.s, self.n))
@@ -46,6 +51,7 @@ class CoEVOL():
         self.U = np.random.rand(self.n, self.k)
         self.X = np.random.rand(self.s, self.n, self.k)
         self.Y = np.random.rand(self.s, self.n, self.k)
+        self.error = np.empty((self.T, self.s), dtype=object) # sparse error matrices
 
         return
 
@@ -57,7 +63,10 @@ class CoEVOL():
         MAX_ITER = 500
 
         # Originally, lambda is found by Line Search
-        lamda = 0.0001
+        lamda = 0.00001
+
+        prev_error = math.inf
+        cur_error = 0.0
 
         for i in range(MAX_ITER):
             dJdU, dJdX, dJdY = self.get_derivatives()
@@ -70,27 +79,32 @@ class CoEVOL():
                 self.X[r] -= np.multiply(lamda, dJdX[r])
                 self.Y[r] -= np.multiply(lamda, dJdY[r])
 
+            # Apply nonnegative projection onto X and Y
+            # i.e. make negative entries zero (similar to ReLU)
+            for r in range(self.s):
+                self.X = self.X.clip(min=0)
+                self.Y = self.Y.clip(min=0)
+
             num_iter += 1
 
             # Report the reconstruction error at the end of the iteration
-            print('Error at the end of iteration {}:'.format(num_iter))
-            sum_error = self.get_reconstruction_error()
-            print("Total Error: {}".format(sum_error))
+            print('At the end of iteration {},'.format(num_iter))
+            cur_error = self.get_reconstruction_error()
+            print("Total Error: {}".format(cur_error))
 
-        # print('Before nonnegative projection')
-        # print(self.X)
-        # print(self.Y)
+            if(abs(cur_error - prev_error) < self.threshold):
+                # Stopping criterion met.
+                print('Convergence is reached. Error is:')
+                print(cur_error)
+                break
+            # Otherwise, continue running
+            prev_error = cur_error
 
-        # Apply nonnegative projection onto X and Y
-        # i.e. make negative entries zero (similar to ReLU)
-        for r in range(self.s):
-            self.X = self.X.clip(min=0)
-            self.Y = self.Y.clip(min=0)
+        print('Execution completed. Error is:')
+        print(cur_error)
+        print('Factorization completed for k = {}, theta = {}.'.format(self.k, self.theta))
 
-        # print('After nonnegative projection')
-        # print(self.X)
-        # print(self.Y)
-        return
+        return self.U, self.X, self.Y
 
     def get_derivatives(self):
         """
@@ -107,11 +121,11 @@ class CoEVOL():
 
                 # print('aaaaaaa')
                 # Inner term for dJdU
-                dJdU += np.dot(error, ( -1 * self.X[r] * t - self.Y[r]) )
+                dJdU += error.dot( -1 * self.X[r] * t - self.Y[r])
 
-                dJdX[r] += np.dot(error.T, (-self.U * t))
+                dJdX[r] += error.transpose().dot(-self.U * t)
 
-                dJdY[r] += np.dot(error.T, (-self.U))
+                dJdY[r] += error.transpose().dot(-self.U)
 
                 # print('bbbbbbb')
 
@@ -139,7 +153,8 @@ class CoEVOL():
         error_over_time = np.zeros(self.T)
         for t in range(self.T):
             for r in range(self.s):
-                error_over_time[t] += np.linalg.norm( self.get_error(t, r) )
+                error_over_time[t] += self.get_rmse(t, r)
+
 
         decayed_error_over_time = self.decay(error_over_time)
 
@@ -149,20 +164,43 @@ class CoEVOL():
         reg_Y = 0.5 * self.gamma * np.linalg.norm(self.Y)
 
         # print(decayed_error_over_time)
-        print(reg_U)
-        print(reg_X)
-        print(reg_Y)
+        # print(reg_U)
+        # print(reg_X)
+        # print(reg_Y)
 
         sum_error = np.linalg.norm(decayed_error_over_time) + reg_U + reg_X + reg_Y
         return sum_error
+
+    def get_rmse(self, t, r):
+        '''
+        For the given snapshot, return RMSE of the latest iteration
+        '''
+        # Get the nonzero indices
+        nnz_ind, _ = self.error[t, r].nonzero()
+
+        # Calculate RMSE, using the number of nonzero indices
+        rmse = sps.linalg.norm(self.error[t, r]) / math.sqrt(len(nnz_ind))
+        return rmse
 
     def get_error(self, t, r):
         """
         Returns the error of subject r at time t. Equation 2.7 in the paper.
         """
-        error = self.A[t, r] - (self.U.dot( (self.X[r] * t + self.Y[r]).T ) )
+        # Get the nonzero indices of the current A matrix. Only for those indices
+        # We should add up errors.
+        nz_i, nz_j = self.A[t,r].nonzero()
 
-        return error
+        err = lil_matrix((self.n, self.n))    # Empty sparse matrix NxN
+        reconst = self.U.dot( (self.X[r] * t + self.Y[r]).T )
+
+        for k in range(len(nz_i)):  # for each nonzero index
+            err[nz_i[k], nz_j[k]] = self.A[t, r][nz_i[k], nz_j[k]] - reconst[nz_i[k], nz_j[k]]
+            self.error[t, r] = err
+
+        # error = self.A[t, r] - (self.U.dot( (self.X[r] * t + self.Y[r]).T ) )
+        # print(sps.linalg.norm(error))
+
+        return self.error[t, r]
 
     def decay(self, V):
         '''
@@ -176,7 +214,7 @@ class CoEVOL():
         for i in range(T):
             V_new[T-1 - i] = V[T-1 - i] * math.pow(math.e, (-i * self.theta)) / 2.0
 
-        print(V_new)
+        # print(V_new)
 
         return V_new
 
@@ -189,9 +227,12 @@ class CoEVOL():
 
 
 # TEST
-A = np.random.randint(2, size=(20, 3, 10, 10))
-print(np.linalg.norm(A))
-print(A)
+# A = np.empty((20, 3), dtype=object)
+# for i in range(20):
+#     for j in range(3):
+#         A[i, j] = csr_matrix([[0,0,1],[0,1,1],[0,0,0]])
 
-coevol = CoEVOL(A, k=4)
-coevol.factorize()
+# print(A[0,0].shape)
+
+# coevol = CoEVOL(A, 3, k=4)
+# coevol.factorize()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # coevol-python
+
 Shared Temporal Matrix Factorization with CoEVOL as described in Yu et al. 2018 - 'Modeling CoEvolution Across Multiple Networks'
+
+Paper: https://epubs.siam.org/doi/pdf/10.1137/1.9781611975321.76
